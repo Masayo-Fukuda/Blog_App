05611e20e7c55c561b19c7a85b9c2769f9a67c09
Masayo-Fukuda <126930383+Masayo-Fukuda@users.noreply.github.com>
2023-05-30 20:48:37 +0900

    edit

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,40 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"></script>
-  <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
-</head>
-<body>
-  <header>
-    <p>Blog App</p>
-  </header>
+  @extends('layouts.app')
+  @section('title', 'Login')
+  @section('body')
+        <div class="title">
+          <p>Edit post</p>
+        </div>
 
-  <div class="title">
-    <p>Edit post</p>
-  </div>
+        <div class="box">
+          <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Title</label>
+            <input type="text" class="form-control" id="exampleFormControlInput1">
+          </div>
+          <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Body</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+        </div>
 
-  <div class="box">
-    <div class="mb-3">
-      <label for="exampleFormControlInput1" class="form-label">Title</label>
-      <input type="text" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mb-3">
-      <label for="exampleFormControlTextarea1" class="form-label">Body</label>
-      <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-    </div>
-  </div>
-
-  <div class="create_button">
-    <button type="button" class="btn btn-outline-secondary">Create</button>
-  </div>
-
-  <footer>
-    <small>Copyright &copy; 2023 Blog App</small>
-  </footer>
-</body>
-</html>
+        <div class="create_button">
+          <button type="button" class="btn btn-outline-secondary">Create</button>
+        </div>
+  @endsection
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,65 +1,52 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Posts</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"></script>
-    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/posts_index.css') }}">
-</head>
-
-<body>
-    <header>
-        <p>Blog App</p>
-    </header>
-
+  @extends('layouts.app')
+  @section('title', 'Login')
+  @section('body')
     <div class="title">
         <p>Everyone's Posts</p>
     </div>
 
     <div class="py-5">
 
-        <div id="post" class="card border-primary-subtle mb-3" style="max-width: 600px;">
-            <div class="card-header bg-transparent border-primary-subtle">User</div>
-            <div class="card-body text-primary-emphasis">
-                <h5 class="card-title">Title</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolores quidem
-                    repellat sint eaque hic, veniam, quasi fuga eum cum </p>
-            </div>
-            <div class="card-footer bg-transparent border-primary-subtle">Date</div>
-        </div>
+      <div class="d-flex justify-content-center">
+        <div id="post" class="card border-primary-subtle mb-3"  style="max-width: 600px;">
+          <div class="card-header bg-transparent border-primary-subtle">User</div>
+          <div class="card-body text-primary-emphasis">
+              <h5 class="card-title">Title</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat dolores quidem
+                  repellat sint eaque hic, veniam, quasi fuga eum cum </p>
+          </div>
+          <div class="card-footer bg-transparent border-primary-subtle">Date</div>
+       </div>
+      </div>
+        
 
+      <div class="d-flex justify-content-center">
         <div id="post" class="card border-primary-subtle mb-3" style="max-width: 600px;">
-            <div class="card-header bg-transparent border-primary-subtle">User</div>
-            <div class="card-body text-primary-emphasis">
-                <h5 class="card-title">Title</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, deleniti
-                    consequuntur corrupti officia ipsam facilis nemo excepturi dicta atque iure. Assumenda, sunt?
-                    Nostrum modi voluptatem</p>
-            </div>
-            <div class="card-footer bg-transparent border-primary-subtle">Date</div>
+          <div class="card-header bg-transparent border-primary-subtle">User</div>
+          <div class="card-body text-primary-emphasis">
+              <h5 class="card-title">Title</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, deleniti
+                  consequuntur corrupti officia ipsam facilis nemo excepturi dicta atque iure. Assumenda, sunt?
+                  Nostrum modi voluptatem</p>
+          </div>
+          <div class="card-footer bg-transparent border-primary-subtle">Date</div>
         </div>
+      </div>
 
+        
+
+      <div class="d-flex justify-content-center">
         <div id="post" class="card border-primary-subtle mb-3" style="max-width: 600px;">
-            <div class="card-header bg-transparent border-primary-subtle">User</div>
-            <div class="card-body text-primary-emphasis">
-                <h5 class="card-title">Title</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, deleniti
-                    consequuntur corrupti officia ipsam facilis nemo excepturi dicta atque iure. Assumenda, sunt?
-                    Nostrum modi voluptatem</p>
-            </div>
-            <div class="card-footer bg-transparent border-primary-subtle">Date</div>
-        </div>
+          <div class="card-header bg-transparent border-primary-subtle">User</div>
+          <div class="card-body text-primary-emphasis">
+              <h5 class="card-title">Title</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum, deleniti
+                  consequuntur corrupti officia ipsam facilis nemo excepturi dicta atque iure. Assumenda, sunt?
+                  Nostrum modi voluptatem</p>
+          </div>
+          <div class="card-footer bg-transparent border-primary-subtle">Date</div>
+      </div>
+      </div>
 
     </div>
-
-    <footer>
-        <small>Copyright &copy; 2023 Blog App</small>
-    </footer>
-</body>
-
-</html>
+  @endsection
