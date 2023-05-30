a8a96e0cfc080e7a749a52fc0388fbdc7c90997f
glennmichael123 <gtorregosa@gmail.com>
2023-05-30 17:19:17 +0800

    glenn changes

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"></script>
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+</head>
+
+<body>
+    <header>
+        <p>Blog App</p>
+    </header>
+
+    @yield('body')
+
+    <footer>
+        <small>Copyright &copy; 2023 Blog App</small>
+    </footer>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ Route::get('login', function () {
     return view('login');
 });
 
+Route::view('login', 'login');
+
 Route::get('register', function () {
     return view('register');
 });
